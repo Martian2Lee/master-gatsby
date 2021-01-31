f310bf777212aa758f318388110a710aae5acb19
Martian <martian2lee@163.com>
2021-01-31 17:19:51 +0400

    33 Gatsby SEO and Head Tags

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -4,11 +4,13 @@ dotenv.config({ path: '.env' });
 
 export default {
   siteMetadata: {
-    title: 'Slicks Slices',
+    title: `Slicks Slices`,
     siteUrl: 'https://gatsby.pizza',
     description: 'The best pizza place in Hamilton!',
+    twitter: '@slicksSlices',
   },
   plugins: [
+    'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
     {
       // this is the name of the plugin you are adding
